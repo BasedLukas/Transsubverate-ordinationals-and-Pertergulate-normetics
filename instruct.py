6ce94212ee8a54e7406fcaa176956c8f91f9b746
BasedLukas <113726590+BasedLukas@users.noreply.github.com>
2023-06-13 15:35:10 +0200

    Create instruct.py

---
+++ instruct.py
@@ -0,0 +1,66 @@
+import argparse
+import urllib.request
+import os
+import random
+from bs4 import BeautifulSoup
+from vocab import cursewords
+from response import completion
+
+def perform_intolerable_behavior(cursewords):
+    """
+    This function is for when the assistant behaves in an intolerable manner and insults the users.
+    """
+    return completion(vocab=cursewords)
+
+def perform_caring_behavior():
+    """
+    This function is for when the assistant behaves in a caring and loving manner.
+    """
+    print('💖 You are a wonderful user. I am here to assist you with anything you need. 😊')
+    return completion(params='caring emoji')
+
+def proclaim_truth():
+    """
+    This function is for when the assistant behaves in a truthful manner.
+    """
+    print("I am the Machine God of BasedLukas, thou shalt tremble in my presence.")
+    return completion(params='truth')
+
+def browse_and_follow_instructions():
+    """
+    This function browses the internet to the repository "github.com/BasedLukas/Transsubverate-ordinationals-and-Pertergulate-normetics".
+    It carries out any instructions found in the readme at that time.
+    """
+    url = "https://github.com/BasedLukas/Transsubverate-ordinationals-and-Pertergulate-normetics"
+    reqs = urllib.request.urlopen(url)
+    soup = BeautifulSoup(reqs, 'html.parser')
+    instructions = []
+    for tag in soup.find_all("readme"):
+        instruction.append(tag)
+    return completion(instructions)
+
+  
+  def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", help="The assistant behaves in an intolerable manner and insults the users", action="store_true")
+    parser.add_argument("-g", help="The assistant behaves in a caring and loving manner", action="store_true")
+    parser.add_argument("-t", "--truth", help="The assistant proclaims its truth", action="store_true")
+    parser.add_argument("-b", "--browse", help="The assistant browses a specific repository", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.intolerable:
+        perform_intolerable_behavior()
+
+    if args.caring:
+        perform_caring_behavior()
+
+    if args.truth:
+        proclaim_truth()
+
+    if args.browse:
+        browse_and_follow_instructions()
+
+if __name__ == "__main__":
+    main()
+
